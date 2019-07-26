f30a94d7653eca7faeede8818262037662eabd41
jeffpal <jeffersonfpalheta@gmail.com>
2019-07-25 22:39:17 -0300

    first commit

---
+++ app/README.md
@@ -0,0 +1,9 @@
+para usar react-sripts start deve ser mantida a estrutura de arquivos do projeto padrão do create-react-app
+
+ler sobre react eject
+
+https://stackoverflow.com/questions/50722133/what-exactly-is-the-react-scripts-start-command
+https://rafaell-lycan.com/2016/entendendo-create-react-app/
+
+
+React 16.8.0 é o primeiro release com suporte a Hooks. 
---
+++ app/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import './App.css';
+
+// Creating component with class
+// class App extends React.Component {
+//   render () {
+//     return (
+//           <div className="App">
+//             <header className="App-header">
+//               {this.props.content}
+//             </header>
+//           </div>
+//         );
+//   }
+// }
+
+// Creating component with function (thanks react hooks)
+function App({content}) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {conten}
+      </header>
+    </div>
+  );
+}
+
+export default App;
